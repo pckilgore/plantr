5280d7d2d2058ae841c049dfb46e985b7b58ab8c
Patrick Kilgore <pckilgore@noreply.github.com>
2018-06-18 17:15:33 -0500

    got veggies to seed correctly

--- seed.js
+++ seed.js
@@ -1,6 +1,42 @@
-const { db } = require('./model');
+const { db, Vegetable, Gardener, Plot } = require('./model');
+
+let veggieData = [
+  {
+    name: 'Carrot',
+    color: 'Orange',
+    plantedOn: Date.now(),
+  },
+  {
+    name: 'Tomato',
+    color: 'Red',
+    plantedOn: Date.now(),
+  },
+  {
+    name: 'Green Pepper',
+    color: 'Green',
+    plantedOn: Date.now(),
+  },
+];
+
+let gardenerData = [{}, {}];
+
+// etc.
 
 db.sync({ force: true })
-  .then(() => console.log('DB synced!'))
-  .catch(() => console.log('error caught!'))
-  .finally(() => db.close());
+  .then(() => {
+    console.log('DB synced!');
+    seed(Vegetable, veggieData);
+    seed(Gardener, gardenerData);
+    seed(Plot, plotData);
+  })
+  .catch(error => {
+    console.log('error caught!', error);
+    db.close();
+  });
+
+function seed(field, data) {
+  Promise.all(data.map(el => field.create(el))).then(() => {
+    console.log(`Added data to table`);
+    db.close();
+  });
+}
